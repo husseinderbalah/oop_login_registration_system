e195ab728956351edccb1b4f15f45b675401c57a
husseinderbalah <e.h.m707@gamil.com>
2018-12-23 23:53:58 +0200

    Creating DB class and database functions get/delete

--- index.php
+++ index.php
@@ -3,11 +3,16 @@ require_once 'core/init.php';
 
 $users =  DB::getInstance(); //CALLING CREATED OBJECT IN DB CLASS
 //$users->query("SELECT * FROM groups WHERE id = ? ", array('5'));
-$users->action('SELECT *','groups',array('id','=','1'));
+//$users->action('SELECT *','groups',array('id','=','1'));
+$users->get('groups',array('id','=','2'));
+
 //echo $users->error() . "<br>";
 
 
-if ($users->error()){
+if (!$users->count()){
     echo "No Users";
 }
-else{echo "OK";}
+else{
+    echo $users->result()[0]->user_name;
+
+}
